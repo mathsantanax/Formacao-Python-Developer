6c26f38f43786f8c04f792086e63559921c83f1a
Matheus-A-Santana <matheus.santana7@hotmail.com>
2023-08-30 22:36:55 -0300

    desafio do tweet

---
+++ Fundamentos de Python/desafio_tweet.py
@@ -0,0 +1,18 @@
+# Este é um projeto desenvolvido atráves dos desafios da dio.me 
+
+# DESAFIO
+# O microblog Twitter é conhecido por limitar as postagens em 140 caracteres. Conferir se um texto vai caber em um tuíte é sua tarefa.
+
+# Entrada
+# A entrada é uma linha de texto T (1 ≤ |T| ≤ 500).
+
+# Saída
+# A saída é dada em uma única linha. Ela deve ser "TWEET" (sem as aspas) se a linha de texto T tem até 140 caracteres.
+# Se T tem mais de 140 caracteres, a saída deve ser "MUTE".
+
+T = input()
+
+if len(T) <= 140:
+    print('TWEET')
+else:
+    print("MUTE")
