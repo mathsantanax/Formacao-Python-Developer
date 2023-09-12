882fb9ced4cc1c21b09b160dbb549959c7717c74
Matheus-A-Santana <matheus.santana7@hotmail.com>
2023-09-11 23:54:03 -0300

    desafio Animal

---
+++ Estrutura de Dados/Animal.py
@@ -0,0 +1,29 @@
+a = input()
+b = input()
+c = input()
+
+if a == 'vertebrado':
+    if b == 'ave':
+        if c == 'carnivoro':
+            print('aguia')
+        elif c == 'onivoro':
+            print('pomba')
+        
+    elif b == 'mamifero':
+        if c == 'onivoro':
+            print('homem')
+        elif c == 'herbivoro':
+            print('vaca')
+
+elif a == 'invertebrado':
+    if b =='inseto':
+        if c == 'hematofago':
+            print('pulga')
+        if c == 'herbivoro':
+            print('lagarta')
+
+    elif b == 'anelideo':
+        if c == 'hematofago':
+            print('sanguessuga')
+        if c == 'onivoro':
+            print('minhoca')
